28da1f706f65448a375fa03cb6bac0b491267f9a
Robert Kunc <rkunc9398@gmail.com>
2021-10-13 10:36:56 +0200

    adding new code to master

--- script.js
+++ script.js
@@ -2,23 +2,20 @@
 
 // SELECTING ELEMENTS
 const player = document.querySelector('.player');
-
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-
 const playerActive = document.querySelector('.player--active');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
 const diceEl = document.querySelector('.dice');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-current0El.textContent = 0;
-current1El.textContent = 0;
+
+// current0El.textContent = 0;
+// current1El.textContent = 0;
 
 // must be outside of the function otherwise iteration would pass over
 let currentScore = 0;
@@ -28,9 +25,19 @@ let activePlayer = 0;
 let playing = true;
 
 // STARTING CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  playing = true;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  diceEl.classList.add('hidden');
+};
+init();
 
 // function SWITCH PLAYER
 const switchPlayer = function () {
@@ -41,7 +48,6 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-// STARTING CONDITIONS
 btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. GENERATE RANDOM ROLL
@@ -93,4 +99,7 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
-document.querySelector('.btn--new').addEventListener('click', function () {});
+
+document.querySelector('.btn--new').addEventListener('click', function () {
+  init();
+});
